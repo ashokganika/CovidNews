7c26968c46bb12750c86c76bb1a6b941397478ae
Mr.A <ashok.ag.ganika@gmail.com>
2020-06-07 18:58:14 +0545

    Updates

---
+++ precache-manifest.1e8150512238b60421242e91a6f3464a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758bdd2130ac0d5cff9f9a8d60f3689a",
+    "url": "/CovidNews/index.html"
+  },
+  {
+    "revision": "a5330dd3e7b174422c12",
+    "url": "/CovidNews/static/css/main.9198c37c.chunk.css"
+  },
+  {
+    "revision": "f95dfbe9501a1d1dbfdd",
+    "url": "/CovidNews/static/js/2.a6a520ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CovidNews/static/js/2.a6a520ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5330dd3e7b174422c12",
+    "url": "/CovidNews/static/js/main.b5a90805.chunk.js"
+  },
+  {
+    "revision": "8602e213fa7e6a94181a",
+    "url": "/CovidNews/static/js/runtime-main.cd632e4b.js"
+  },
+  {
+    "revision": "946910c6cf042338840ea2b824dc4652",
+    "url": "/CovidNews/static/media/Group.946910c6.svg"
+  },
+  {
+    "revision": "919e4e5ca5d4580d3cecfdd79ed18d6c",
+    "url": "/CovidNews/static/media/arrow_back.919e4e5c.svg"
+  },
+  {
+    "revision": "f40b7cfaf20bd16786cccf32c47e1e6f",
+    "url": "/CovidNews/static/media/arrow_btn.f40b7cfa.svg"
+  },
+  {
+    "revision": "0f47bdc705316705c5d3dac8c1b1fba3",
+    "url": "/CovidNews/static/media/bullhorn.0f47bdc7.svg"
+  },
+  {
+    "revision": "1b92b64da7e7bb6e16987302821e2f5a",
+    "url": "/CovidNews/static/media/contactt.1b92b64d.svg"
+  },
+  {
+    "revision": "502d24f09e5103e64b0e62b0dcf015bd",
+    "url": "/CovidNews/static/media/covid.502d24f0.jpg"
+  },
+  {
+    "revision": "a5a381ce1f6ad3d69497286f516af676",
+    "url": "/CovidNews/static/media/details.a5a381ce.svg"
+  },
+  {
+    "revision": "2c5785f81e4fdc287077c5bf86d94830",
+    "url": "/CovidNews/static/media/ekantipur.2c5785f8.jpg"
+  },
+  {
+    "revision": "3fcc5740231e021d76870e44ec4bd91c",
+    "url": "/CovidNews/static/media/event.3fcc5740.svg"
+  },
+  {
+    "revision": "5b7dc716aacc8248402a263436ac070a",
+    "url": "/CovidNews/static/media/know_more.5b7dc716.png"
+  },
+  {
+    "revision": "03ac8eb5a2d01cf8537607772544ebae",
+    "url": "/CovidNews/static/media/location.03ac8eb5.svg"
+  },
+  {
+    "revision": "9d55817d8e3cb962e90f37db69d537e9",
+    "url": "/CovidNews/static/media/message.9d55817d.svg"
+  },
+  {
+    "revision": "bad96b6a68fb6761083c335246ba28b3",
+    "url": "/CovidNews/static/media/more_vert.bad96b6a.svg"
+  },
+  {
+    "revision": "669fab311ddf9e26be418a97755d2241",
+    "url": "/CovidNews/static/media/phone-call.669fab31.svg"
+  },
+  {
+    "revision": "33048e8fdf8c7e9f44ca391aa89c6c73",
+    "url": "/CovidNews/static/media/read_news.33048e8f.svg"
+  },
+  {
+    "revision": "c3ae7b33176ce54978a91db90b361e70",
+    "url": "/CovidNews/static/media/report.c3ae7b33.svg"
+  },
+  {
+    "revision": "a035edf2bbe21c2915cc115b7456c941",
+    "url": "/CovidNews/static/media/stay_alert.a035edf2.svg"
+  },
+  {
+    "revision": "e20fe41a66a52d1547635256d6306f38",
+    "url": "/CovidNews/static/media/stay_updated.e20fe41a.svg"
+  },
+  {
+    "revision": "5f871fb8c5d2e5baacaa7ab30e14b6e3",
+    "url": "/CovidNews/static/media/survey.5f871fb8.svg"
+  }
+]);
